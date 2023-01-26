b78e924d69a8e12be847201183f2636445a56ce7
Enzo-Ga <93016718+Enzo-Ga@users.noreply.github.com>
2023-01-26 18:01:00 -0300

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default class Profile extends Component {
+  render() {
+    return <Text>Profile</Text>;
+  }
+}
